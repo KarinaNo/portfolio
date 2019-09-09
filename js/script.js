d44bccf901e26d9536c73981bea20f4c3516a059
Karina Nowrot <karina.nowrot@gmail.com>
2019-09-09 14:07:49 +0200

    Update2

--- js/script.js
+++ js/script.js
@@ -6,14 +6,15 @@ function myFunction() {
 window.addEventListener('scroll', () => {
     let slide = document.getElementById('home');
     let slideDiv = slide.getElementsByTagName('div');
+    let heightWindow = window.innerWidth;
     for (let i = 0; i < slideDiv.length; i++) {
         console.log(i);
+        home
         slideDiv[i].style.transform = 'translateY(-' + (window.pageYOffset * i / slideDiv.length) + 'px)';
         if (i == 2) {
             slideDiv[i].style.transform += 'rotate(-' + (window.pageYOffset / 50) + 'deg)';
         } else if (i == 4) {
             slideDiv[i].style.transform += 'rotate(' + (window.pageYOffset / 20) + 'deg) scale(1.' + (window.pageYOffset / 50) + ')';
-
         }
     }
 
